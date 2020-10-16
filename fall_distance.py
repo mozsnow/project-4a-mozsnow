556af49b890fac94c0b6125d94fb4a2c4c31d264
mozsnow <67405205+mozsnow@users.noreply.github.com>
2020-10-16 14:09:41 -0400

    fall

--- fall_distance.py
+++ fall_distance.py
@@ -3,6 +3,6 @@ def fall_distance(time):
     distance = 0.5 *gravity *(time ** 2)
     return distance
 print('\n' + '\033[4m' + 'Time (s) Distance Fallen (m)' + '\033[0m')
-for seconds in range(1,7):
+for seconds in range(1,11):
     distance_fallen = fall_distance(seconds)
     print(' ' + format(seconds, '<d'), format(distance_fallen, '10.1f'))
